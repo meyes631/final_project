94a89047d11cd4b36a64dfca49281ee10da7ace6
meyes631 <meyes631@gmail.com>
2015-12-04 16:38:46 -0500

    edits

---
+++ helpers/p5.js
@@ -0,0 +1,27 @@
+
+<html>
+
+	<head>
+		<title> My Final Project</title>
+	</head>
+
+	<body>
+
+
+		<h1>This is my final project</h1>
+
+
+		FORLOOP EXAMPLE
+
+
+		background ("white");
+
+		for(var i = 0; i <10 ; i++)
+			elipse(random(0, width), random(0,height), 50,50)
+
+
+
+
+	</body>
+
+</html>
