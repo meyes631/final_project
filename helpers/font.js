3913ead11082d649d66701f3612176f96ec588e5
meyes631 <meyes631@gmail.com>
2015-12-18 13:38:39 -0500

    all

--- helpers/font.js
+++ helpers/font.js
@@ -1,3 +1,7 @@
+
+
+// SMALL STILL LETTERS
+
 $('.letter').hover(function() {
     // mouse in
     $(this).addClass('show');
@@ -10,3 +14,604 @@ $('.letter').hover(function() {
     }
     
 });
+
+// LARGE FLICKERING LETTERS
+
+$('.letterC').hover(function() {
+    // mouse in
+    $(this).removeClass('show');
+}, function() {
+    // mouse out
+    var keep = $(this).data('keep');
+    
+    if (!keep) {
+        $(this).addClass('show');
+    }
+    
+});
+
+// SMALL FLICKERING LETTERS
+
+$('.letterS').hover(function() {
+    // mouse in
+    $(this).removeClass('show');
+}, function() {
+    // mouse out
+    var keep = $(this).data('keep');
+    
+    if (!keep) {
+        $(this).addClass('show');
+    }
+    
+});
+
+
+
+
+// var key 
+	 // console.log("hello");
+	 function setup(){
+
+	 	noCanvas();
+	 }
+
+
+
+
+
+
+function keyTyped() {
+	 console.log("key");
+  if (key === 'a') {
+
+var letterA = select("#keyA");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyA'); 
+
+
+    console.log("a");
+  } else if (key === 'b') {
+    select('#keyB');
+    console.log("b");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'b') {
+
+var letterA = select("#keyB");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyB');
+
+
+    console.log("b");
+  } else if (key === 'c') {
+    select('#keyC');
+    console.log("c");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'c') {
+
+var letterA = select("#keyC");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyC');
+
+
+    console.log("c");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'd') {
+
+var letterA = select("#keyD");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyD');
+
+
+    console.log("d");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'e') {
+
+var letterA = select("#keyE");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keye');
+
+
+    console.log("e");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'f') {
+
+var letterA = select("#keyF");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyF');
+
+
+    console.log("f");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'g') {
+
+var letterA = select("#keyG");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyG');
+
+
+    console.log("g");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'h') {
+
+var letterA = select("#keyH");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyH');
+
+
+    console.log("h");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'i') {
+
+var letterA = select("#keyI");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyI');
+
+
+    console.log("i");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'j') {
+
+var letterA = select("#keyJ");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyj');
+
+
+    console.log("j");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'k') {
+
+var letterA = select("#keyK");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyK');
+
+
+    console.log("k");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'l') {
+
+var letterA = select("#keyL");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyL');
+
+
+    console.log("l");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'm') {
+
+var letterA = select("#keyM");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyM');
+
+
+    console.log("m");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'n') {
+
+var letterA = select("#keyN");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyN');
+
+
+    console.log("n");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'o') {
+
+var letterA = select("#keyO");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyO');
+
+
+    console.log("o");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'p') {
+
+var letterA = select("#keyP");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyP');
+
+
+    console.log("p");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'q') {
+
+var letterA = select("#keyQ");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyQ');
+
+
+    console.log("q");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'r') {
+
+var letterA = select("#keyR");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyR');
+
+
+    console.log("r");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 's') {
+
+var letterA = select("#keyS");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyS');
+
+
+    console.log("s");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 't') {
+
+var letterA = select("#keyT");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyT');
+
+
+    console.log("t");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'u') {
+
+var letterA = select("#keyU");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyU');
+
+
+    console.log("u");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'v') {
+
+var letterA = select("#keyV");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyV');
+
+
+    console.log("v");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'w') {
+
+var letterA = select("#keyW");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyW');
+
+
+    console.log("w");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'x') {
+
+var letterA = select("#keyX");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyX');
+
+
+    console.log("x");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'y') {
+
+var letterA = select("#keyY");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyY');
+
+
+    console.log("y");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === 'z') {
+
+var letterA = select("#keyZ");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyZ');
+
+
+    console.log("z");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+
+  if (key === ' ') {
+
+var letterA = select("#keySP");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keySP');
+
+
+    console.log(" ");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+console.log(key)
+
+  if (key === '.') {
+
+var letterA = select("#keyPER");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyPER');
+
+
+    console.log(".");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === ',') {
+
+var letterA = select("#keyCOM");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyCOM');
+
+
+    console.log(",");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+  if (key === ':') {
+
+var letterA = select("#keyCOL");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyCOL');
+
+
+    console.log(":");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+//   return false; // prevent any default behavior
+// }
+
+
+  if (key === ';') {
+
+var letterA = select("#keySEMICOL");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keySEMICOL');
+
+
+    console.log(";");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+// //   return false; // prevent any default behavior
+// // }
+
+
+  if (key === '!') {
+
+var letterA = select("#keyEXC");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyEXC');
+
+
+    console.log("!");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+// //   return false; // prevent any default behavior
+// // }
+
+
+  if (key === '?') {
+
+var letterA = select("#keyQUES");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyQUES');
+
+
+    console.log("?");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+// //   return false; // prevent any default behavior
+// // }
+
+
+  if (key === '-') {
+
+var letterA = select("#keyDASH");
+var myDiv = createDiv(letterA.html() );
+	myDiv.id('keyDASH');
+
+
+    console.log("-");
+  } else if (key === 'a') {
+    select('#keyA');
+    console.log("a");
+  }
+
+  
+// //   return false; // prevent any default behavior
+// // }
+
+
+//   if (key === '') {
+
+// var letterA = select("#keySP");
+// var myDiv = createDiv(letterA.html() );
+// 	myDiv.id('keySP');
+
+
+//     console.log(" ");
+//   } else if (key === 'a') {
+//     select('#keyA');
+//     console.log("a");
+//   }
+// // //   return false; // prevent any default behavior
+// // // }
+myDiv.parent('canvas')
+  return false; // prevent any default behavior
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
